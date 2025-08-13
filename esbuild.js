f72f7ae444171af23171865c5832b59f88c3f117
n622 <k-osawa@prox-industries.jp>
2025-08-14 04:44:48 +0900

    update

--- esbuild.js
+++ esbuild.js
@@ -1,5 +1,8 @@
 import esbuild from "esbuild";
 import { GasPlugin } from "esbuild-gas-plugin";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 esbuild
   .build({
@@ -8,6 +11,11 @@ esbuild
     minify: true,
     outfile: "./dist/main.js",
     plugins: [GasPlugin],
+    define: {
+      "process.env.DISCORD_WEBHOOK_URL": JSON.stringify(
+        process.env.DISCORD_WEBHOOK_URL
+      ),
+    },
   })
   .catch((error) => {
     console.log("ビルドに失敗しました");
---
+++ src/environment.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly DISCORD_WEBHOOK_URL: string;
+  }
+}
